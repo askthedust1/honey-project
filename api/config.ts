f02afba6d9761f956823d0f41265f85945aa2992
Victoria <124435946+askthedust1@users.noreply.github.com>
2023-10-27 12:17:51 +0600

    HONEY-1 dev branch created

--- api/config.ts
+++ api/config.ts
@@ -5,7 +5,7 @@ const rootPath = __dirname;
 const config = {
     rootPath,
     publicPath: path.join(rootPath, "public"),
-    db: "mongodb://localhost/cat",
+    db: "mongodb://localhost/honey",
 };
 
 export default config;
