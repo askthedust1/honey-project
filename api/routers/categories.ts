12f4b10154fca974b52bf41af579d317e8625ed1
Shakirov Rustam <rustam.shakirov.2016@list.ru>
2023-10-27 20:22:48 +0600

    HONEY-17 created category deletion

--- api/routers/categories.ts
+++ api/routers/categories.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import Category from "../models/Category";
 import mongoose from "mongoose";
+import auth from "../middleware/auth";
+import permit from "../middleware/permit";
 
 const categoriesRouter = express.Router();
 
@@ -30,6 +32,25 @@ categoriesRouter.get('/', async (req, res) => {
         return res.send(categories);
     } catch(e) {
         return res.sendStatus(500);
+    }
+});
+
+categoriesRouter.delete("/:id", auth, permit("admin"), async (req, res, next) => {
+    try {
+        const category_id = req.params.id;
+        const category = await Category.findOne({ _id: category_id });
+
+        if (!category) {
+            return res.status(404).send({ error: "Not found!" });
+        }
+
+        await Category.deleteOne({ _id: category_id });
+        return res.send("Category deleted");
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+        return next(error);
     }
 });
 
