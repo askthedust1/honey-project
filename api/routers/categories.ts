85b1a15f9b384a525260e229fd864df62db27b1c
karinami <mkf1230@gmail.com>
2023-10-27 20:48:57 +0600

    HONEY-18 Create PUT request for categories.ts

--- api/routers/categories.ts
+++ api/routers/categories.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import Category from "../models/Category";
 import mongoose from "mongoose";
+import auth from "../middleware/auth";
+import permit from "../middleware/permit";
 
 const categoriesRouter = express.Router();
 
@@ -24,11 +26,34 @@ categoriesRouter.post('/', async (req, res, next) => {
     }
 });
 
+categoriesRouter.put('/:id', auth, permit("admin"), async (req, res, next) => {
+    try {
+        const categoryId = req.params.id;
+        const category = await Category.findById(categoryId);
+
+        if (!category) {
+            return res.status(404).send({error: "Not found!"});
+        }
+
+        category.title = req.body.title || category.title;
+        category.description = req.body.description || category.description;
+
+        await category.save();
+
+        return res.send(category);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+        return next(error);
+    }
+});
+
 categoriesRouter.get('/', async (req, res) => {
     try {
         const categories = await Category.find();
         return res.send(categories);
-    } catch(e) {
+    } catch (e) {
         return res.sendStatus(500);
     }
 });
