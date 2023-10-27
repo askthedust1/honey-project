1d31b0abda22bfc43e2623e8043ed62b8cde8952
arseny <aasolomaha@gmail.com>
2023-10-27 17:49:41 +0600

    HONEY-14 Created an endpoint for login user

--- api/routers/users.ts
+++ api/routers/users.ts
@@ -23,4 +23,27 @@ usersRouter.delete('/sessions', async (req, res, next) => {
     }
 });
 
+usersRouter.post('/sessions', async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(400).send({ error: 'Wrong password or email!' });
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if (!isMatch) {
+            return res.status(400).send({ error: 'Wrong password or email!' });
+        }
+
+        user.generateToken();
+        await user.save();
+
+        return res.send({ message: 'Email and password correct!', user });
+    } catch (e) {
+        next(e);
+    }
+});
+
 export default usersRouter;
