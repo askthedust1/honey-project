20702a79234c3a183ee62478af09068ca9116520
Zobnin Boris <singulthor@gmail.com>
2023-10-27 16:16:57 +0600

    HONEY-11 Create category post router

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import config from "./config";
+import categoriesRouter from "./routers/categories";
 
 const app = express();
 const port = 8000;
@@ -9,7 +10,7 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use('/categories', categoriesRouter);
 const run = async () => {
     await mongoose.connect(config.db);
 
---
+++ api/routers/categories.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import Category from "../models/Category";
+import mongoose from "mongoose";
+
+const categoriesRouter = express.Router();
+
+categoriesRouter.post('/', async (req, res, next) => {
+
+    const categoryData = {
+        title: req.body.title,
+        description: req.body.description,
+    };
+
+    const category = new Category(categoryData);
+
+    try {
+        await category.save();
+        res.send(category);
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(e);
+        }
+        next(e);
+    }
+});
+
+export default categoriesRouter;
