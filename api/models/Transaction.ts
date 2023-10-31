14d1a662043c90221ee1d26e6d1590c24310d083
Shakirov Rustam <rustam.shakirov.2016@list.ru>
2023-10-31 12:48:25 +0600

    HONEY-21 created transaction model

---
+++ api/models/Transaction.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import Product from "./Product";
+import User from "./User";
+
+const TransactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        validate: {
+            validator: async (value: mongoose.Types.ObjectId) => await User.findById(value),
+            message: 'User does not exist',
+        }
+    },
+
+    kits: [ {
+        product: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+            validate: {
+                validator: async (value: mongoose.Types.ObjectId) => await Product.findById(value),
+                message: 'Product does not exist',
+            }
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+    } ],
+
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+
+    status: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+const Transaction = mongoose.model('Transaction', TransactionSchema)
+
+export default Transaction;
