24e55783d148eeadbb4617e8430e1f747b1a7307
arseny <aasolomaha@gmail.com>
2023-11-08 14:57:42 +0600

    Honey-72 Created footer component

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import {wrapper} from "@/store/store";
 import {Provider} from "react-redux";
+import Footer from "@/components/UI/footer/Footer";
 
 export default function App({Component, ...rest}: AppProps) {
   const {store, props} = wrapper.useWrappedStore(rest);
   return (
       <Provider store={store}>
         <Component {...props.pageProps} />
+        <Footer/>
       </Provider>
   )
 }
