965bcf69840e4e7c433fc0d72d9cca634b6882f5
karinami <mkf1230@gmail.com>
2023-11-08 22:04:44 +0600

    HONEY-73 Updated GET one product in api

--- api/routers/products.ts
+++ api/routers/products.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import {imagesUpload} from '../multer';
+import { imagesUpload } from '../multer';
 import auth from '../middleware/auth';
 import mongoose from 'mongoose';
-import {IProductPost} from '../types';
+import { IProductPost } from '../types';
 import Product from '../models/Product';
 import Category from '../models/Category';
 import permit from '../middleware/permit';
@@ -15,7 +15,7 @@ const productRouter = express.Router();
 productRouter.get('/', async (req, res) => {
   try {
     const token = req.get('Authorization');
-    const user = await User.findOne({token});
+    const user = await User.findOne({ token });
 
     if (user && user.role === 'admin') {
       const result = await Product.find();
@@ -23,11 +23,11 @@ productRouter.get('/', async (req, res) => {
     }
 
     if (req.query.category) {
-      const result = await Product.find({category: req.query.category, isActive: true});
+      const result = await Product.find({ category: req.query.category, isActive: true });
       return res.send(result);
     }
 
-    const products = await Product.find({isActive: true});
+    const products = await Product.find({ isActive: true });
     return res.send(products);
   } catch (error) {
     console.error('Error fetching products:', error);
@@ -37,17 +37,29 @@ productRouter.get('/', async (req, res) => {
 
 productRouter.get('/:id', async (req, res) => {
   try {
-    const product = await Product.findById(req.params.id).populate({
+    const token = req.get('Authorization');
+    const user = await User.findOne({ token });
+
+    const productId = req.params.id;
+    const product = await Product.findById(productId).populate({
       path: 'category',
       select: ['title'],
       model: Category,
     });
 
     if (!product) {
-      return res.sendStatus(404);
+      return res.status(404).send({ error: 'Not found' });
     }
 
-    return res.send(product);
+    if (
+      (user && user.role === 'admin') ||
+      (!user && product.isActive) ||
+      (user && user.role === 'user' && product.isActive)
+    ) {
+      return res.send(product);
+    }
+
+    return res.status(404).send({ error: 'Not found' });
   } catch {
     return res.sendStatus(500);
   }
@@ -59,8 +71,8 @@ productRouter.post(
   permit('admin'),
   imagesUpload.single('image'),
   async (req, res, next) => {
-    if (!req.body.title) return res.status(400).send({error: 'Title is required!'});
-    if (!req.body.price) return res.status(400).send({error: 'Price is required!'});
+    if (!req.body.title) return res.status(400).send({ error: 'Title is required!' });
+    if (!req.body.price) return res.status(400).send({ error: 'Price is required!' });
 
     const productData: IProductPost = {
       category: req.body.category,
