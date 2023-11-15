3e1f4057175e1836c335bb859f7523234ddabef7
Victoria <124435946+askthedust1@users.noreply.github.com>
2023-11-15 13:03:57 +0600

    HONEY-95 add fixtures

--- front/src/components/home/homeOpener/HomeOpener.tsx
+++ front/src/components/home/homeOpener/HomeOpener.tsx
@@ -6,17 +6,10 @@ import {selectBanners} from "@/features/banners/bannersSlice";
 
 const HomeOpener = () => {
   const banners = useAppSelector(selectBanners);
-  console.log(banners)
 
   return (
     <div className={cls.opener}>
         <ImageSlider images={banners}/>
-      {/*<div className={cls.opener_item}>*/}
-      {/*  <h2 className={cls.opener_item_title}>{t('titleOpened')}</h2>*/}
-      {/*  <p className={cls.opener_item_content}>{t('contentOpened')}</p>*/}
-      {/*  <button className={cls.opener_item_btn}>{t('btnOpened')}</button>*/}
-      {/*</div>*/}
-      {/*<img src={jar.src} alt={'jar'} />*/}
     </div>
   );
 };
