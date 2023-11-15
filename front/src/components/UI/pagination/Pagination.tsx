dd8994f9d2c181329b27cecfd6ed695b969f77e3
Zobnin Boris <singulthor@gmail.com>
2023-11-15 14:24:41 +0600

    HONEY-45- Basic Pagination (frontend)

---
+++ front/src/components/UI/pagination/Pagination.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import cls from './pagination.module.scss';
+import Link from "next/link";
+import {useAppSelector} from "@/store/hook";
+import {selectCurrentPage, selectTotalPages} from "@/features/products/productsSlice";
+
+const Pagination = () => {
+
+    const currentPageState = useAppSelector(selectCurrentPage);
+    const totalPageState = useAppSelector(selectTotalPages);
+
+    return (
+        <div className={cls.pagination_block}>
+            <div className={cls.pagination_inner}>
+
+                {/*<button className={cls.previous}>&laquo; Назад</button>*/}
+                {/*<button className={cls.next}>Вперед &raquo;</button>*/}
+
+                {currentPageState > 1 ? <Link
+                    className={cls.previos}
+                    href={'/products/page/' +
+                        (currentPageState > 1 ? currentPageState - 1 : currentPageState)
+                            .toString()}
+                >
+                    &laquo; Назад
+                </Link> :
+                    <Link href={'/products/page/' + currentPageState
+                    .toString()}>Stop</Link>}
+
+                {
+                    currentPageState === totalPageState ? <Link href={'/products/page/' + totalPageState
+                            .toString()}>Stop</Link> :
+                        <Link className={cls.next}
+                              href={'/products/page/' + (currentPageState === totalPageState ? totalPageState
+                                  : currentPageState + 1).toString()}
+                        >Вперед &raquo;
+                        </Link>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Pagination;
