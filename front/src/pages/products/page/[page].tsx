e78031d63a9a757d6ac9e6c72663c0512af3ba05
admins <ufkz200147@gmail.com>
2023-11-16 13:26:07 +0600

    HONEY-103 Added multilingual functionality to the product page

--- front/src/pages/products/page/[page].tsx
+++ front/src/pages/products/page/[page].tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import ProductsAll from "@/features/products/ProductsAll";
-import {wrapper} from "@/store/store";
-import {fetchProducts} from "@/features/products/productsThunk";
-import Pagination from "@/components/UI/pagination/Pagination";
-import {useAppSelector} from "@/store/hook";
-import {selectCurrentPage, selectTotalPages} from "@/features/products/productsSlice";
+import ProductsAll from '@/features/products/ProductsAll';
+import { wrapper } from '@/store/store';
+import { fetchProducts } from '@/features/products/productsThunk';
+import Pagination from '@/components/UI/pagination/Pagination';
+import { useAppSelector } from '@/store/hook';
+import { selectTotalPages } from '@/features/products/productsSlice';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const ProductPage = () => {
+  // const currentPageState = useAppSelector(selectCurrentPage);
+  const totalPagesState = useAppSelector(selectTotalPages);
+  console.log(totalPagesState);
 
-    const currentPageState = useAppSelector(selectCurrentPage);
-    const totalPagesState = useAppSelector(selectTotalPages);
-    console.log(totalPagesState);
-
-    return (
-        <>
-            <ProductsAll/>
-            {totalPagesState > 0 ? <Pagination/> : <></> }
-        </>
-    );
+  return (
+    <>
+      <ProductsAll />
+      {totalPagesState > 0 ? <Pagination /> : <></>}
+    </>
+  );
 };
-
 export const getServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
-    const currentPage = context.params?.page;
-    await store.dispatch(fetchProducts(currentPage as string));
-    return {props: {}};
+  const currentPage = context.params?.page;
+  const { locale } = context;
+
+  await store.dispatch(fetchProducts(currentPage as string));
+  return {
+    props: {
+      name: 'Products',
+      ...(await serverSideTranslations(locale ?? 'ru', ['common', 'header', 'footer'])),
+    },
+  };
 });
 
 export default ProductPage;
