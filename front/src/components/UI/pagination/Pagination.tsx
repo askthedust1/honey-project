33bb0b726d22101e5f1ac2b1ce29917d420bce98
Zobnin Boris <singulthor@gmail.com>
2023-11-16 20:17:13 +0600

    HONEY-104-Upgrade pagination styles and functionality.

--- front/src/components/UI/pagination/Pagination.tsx
+++ front/src/components/UI/pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import cls from './pagination.module.scss';
 import Link from "next/link";
 import {useAppSelector} from "@/store/hook";
@@ -13,26 +13,42 @@ const Pagination = () => {
         <div className={cls.pagination_block}>
             <div className={cls.pagination_inner}>
 
-                {currentPageState > 1 ? <Link
-                    className={cls.previos}
-                    href={'/products/page/' +
-                        (currentPageState > 1 ? currentPageState - 1 : currentPageState)
-                            .toString()}
-                >
-                    &laquo; Назад
-                </Link> :
-                    <Link href={'/products/page/' + currentPageState
-                    .toString()}>Stop</Link>}
+                <div className={cls.pagination_inner}>
 
-                {
-                    currentPageState === totalPageState ? <Link href={'/products/page/' + totalPageState
-                            .toString()}>Stop</Link> :
-                        <Link className={cls.next}
-                              href={'/products/page/' + (currentPageState === totalPageState ? totalPageState
-                                  : currentPageState + 1).toString()}
-                        >Вперед &raquo;
-                        </Link>
-                }
+
+                    {currentPageState > 1 ? <div className={cls.circle_left}><Link
+                            className={cls.previous}
+                            href={'/products/page/' +
+                                (currentPageState > 1 ? currentPageState - 1 : currentPageState)
+                                    .toString()}
+                        >
+                            &#8249;
+                        </Link></div> :
+                        <div className={cls.circle_transparent}><span className={cls.span_text}>&#8249;</span></div>
+                    }
+
+
+                    <div className={cls.circle}>
+                        <div className="inner-circle">{currentPageState} / {totalPageState}</div>
+                    </div>
+
+
+                    {
+                        currentPageState === totalPageState ?
+                            <div className={cls.circle_transparent}>
+                                <span className={cls.span_text}>&#8250;</span>
+                            </div> :
+                            <div className={cls.circle_right}>
+                                <Link className={cls.next}
+                                      href={'/products/page/' + (currentPageState === totalPageState ? totalPageState
+                                          : currentPageState + 1).toString()}
+                                >&#8250;
+                                </Link>
+                            </div>
+                    }
+
+
+                </div>
             </div>
         </div>
     );
