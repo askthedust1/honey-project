4631541f82ee6841213b03fbcfcc870c1df268f3
karinami <mkf1230@gmail.com>
2023-11-16 21:26:38 +0600

    HONEY-99 Added some markup & styles for About us

--- api/models/Banner.ts
+++ api/models/Banner.ts
@@ -3,15 +3,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const BannerSchema = new Schema({
-    image: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
 });
 
 const Banner = mongoose.model('Banner', BannerSchema);
