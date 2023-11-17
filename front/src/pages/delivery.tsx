20d482183714f367d0296379d104cce7a21aa5ff
karinami <mkf1230@gmail.com>
2023-11-17 13:45:04 +0600

    HONEY-106 Added some animation for Contacts page

--- front/src/pages/delivery.tsx
+++ front/src/pages/delivery.tsx
@@ -3,12 +3,14 @@ import bee_delivery from '@/assets/images/bee-delivery.png';
 import { wrapper } from '@/store/store';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import cls from '@/styles/delivery.module.scss';
+import { useTranslation } from 'next-i18next';
 
 const Delivery = () => {
+  const { t } = useTranslation('common');
   return (
     <div className={cls.delivery}>
       <div className={cls.container}>
-        <h1 className={cls.delivery_title}>Delivery</h1>
+        <h1 className={cls.delivery_title}>{t('delivery')}</h1>
         <p className={cls.delivery_info}>Тут пока пусто, но есть переход на страницу)</p>
         <div className={cls.delivery_media}>
           <img src={bee_delivery.src} alt="Delivery" />
