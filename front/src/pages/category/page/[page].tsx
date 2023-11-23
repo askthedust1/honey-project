29c18e44c3f884ee69334e0c33f9d614532289ed
karinami <mkf1230@gmail.com>
2023-11-23 22:33:45 +0600

    HONEY-128 Added som legic in Header.tsx & css styles

--- front/src/pages/category/page/[page].tsx
+++ front/src/pages/category/page/[page].tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import ProductsAll from "@/features/products/ProductsAll";
-import {wrapper} from "@/store/store";
-import {fetchProductsByCategory} from "@/features/products/productsThunk";
-import {useAppSelector} from "@/store/hook";
-import {selectTotalPages} from "@/features/products/productsSlice";
-import {IQueryObjectCategory} from "@/types";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
-import {useRouter} from "next/router";
-import Pagination from "@/components/UI/pagination/Pagination";
-import {fetchCategories} from "@/features/categories/categoriesThunk";
-
+import ProductsAll from '@/features/products/ProductsAll';
+import { wrapper } from '@/store/store';
+import { fetchProductsByCategory } from '@/features/products/productsThunk';
+import { useAppSelector } from '@/store/hook';
+import { selectTotalPages } from '@/features/products/productsSlice';
+import { IQueryObjectCategory } from '@/types';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useRouter } from 'next/router';
+import Pagination from '@/components/UI/pagination/Pagination';
+import { fetchCategories } from '@/features/categories/categoriesThunk';
 
 const ProductByCategoryPage = () => {
   const router = useRouter();
@@ -19,8 +18,15 @@ const ProductByCategoryPage = () => {
   return (
     <>
       <ProductsAll />
-      {totalPagesState > 0 ? <Pagination productsActive={false} categoriesActive={true} idCategory={String(router.query.cId)}/> : <></>}
-
+      {totalPagesState > 0 ? (
+        <Pagination
+          productsActive={false}
+          categoriesActive={true}
+          idCategory={String(router.query.cId)}
+        />
+      ) : (
+        <></>
+      )}
     </>
   );
 };
