8478972a5ea29a1d8de27feafca5c18b5dbb3a42
karinami <mkf1230@gmail.com>
2023-11-23 17:57:14 +0600

    HONEY-112 Added logic for Thunk, Slice & Bestseller.tsx

--- front/src/components/home/bestseller/Bestseller.tsx
+++ front/src/components/home/bestseller/Bestseller.tsx
@@ -1,41 +1,39 @@
 import React from 'react';
-import cls from '../../../styles/_bestseller.module.scss'
-import best_i1 from '@/assets/images/best-img1.png'
-import best_i2 from '@/assets/images/best-img2.png'
-import best_i3 from '@/assets/images/best-img3.png'
-import {useTranslation} from "next-i18next";
+import cls from '../../../styles/_bestseller.module.scss';
+import { useTranslation } from 'next-i18next';
+import { IProduct } from '@/types';
+import ProductItem from '@/features/products/components/ProductItem';
+import { useAppDispatch, useAppSelector } from '@/store/hook';
+import { selectBestsellers } from '@/features/products/productsSlice';
+import { fetchBestsellers } from '@/features/products/productsThunk';
 
 const Bestseller = () => {
-    const { t } = useTranslation('home');
-    return (
-        <div className={cls.bestseller}>
-            <div className={cls.bestseller_container}>
-                <div className={cls.bestseller_btns}>
-                    <button className={cls.bestseller_btns_btn1}>{t('bestsellerBtn1')}</button>
-                    <button className={cls.bestseller_btns_btn2}>{t('bestsellerBtn2')}</button>
-                </div>
+  const { t } = useTranslation('home');
+  const bestsellers = useAppSelector(selectBestsellers);
+  const dispatch = useAppDispatch();
 
-                <div className={cls.bestseller_cards}>
-                    <div className={cls.bestseller_cards_card}>
-                        <img src={best_i1.src} alt={'product1'}/>
-                        <p>ANGEL'S GIFT THYME</p>
-                        <span>500 KGS</span>
-                    </div>
-                    <div className={cls.bestseller_cards_card}>
-                        <img src={best_i2.src} alt={'product2'}/>
-                        <p>MOUNTAIN HONEY POLYFLOR</p>
-                        <span>500 KGS</span>
-                    </div>
-                    <div className={cls.bestseller_cards_card}>
-                        <img src={best_i3.src} alt={'product3'}/>
-                        <p>WHITE HONEY SAINFOIN</p>
-                        <span>500 KGS</span>
-                    </div>
-
-                </div>
-            </div>
+  const getBest = (query: string) => {
+    dispatch(fetchBestsellers(query));
+  };
+  return (
+    <div className={cls.bestseller}>
+      <div className={cls.bestseller_container}>
+        <div className={cls.bestseller_btns}>
+          <button onClick={() => getBest('hit')} className={cls.bestseller_btns_btn1}>
+            {t('bestsellerBtn1')}
+          </button>
+          <button onClick={() => getBest('new')} className={cls.bestseller_btns_btn2}>
+            {t('bestsellerBtn2')}
+          </button>
         </div>
-    );
+        <div className={cls.list}>
+          {bestsellers.map((el: IProduct) => (
+            <ProductItem key={el._id} product={el} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Bestseller;
--- front/src/features/products/productsThunk.ts
+++ front/src/features/products/productsThunk.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosApi from '@/axiosApi';
-import { IProductsOfPage, IProductView , IQueryObjectCategory} from '@/types';
+import { IProduct, IProductsOfPage, IProductView, IQueryObjectCategory } from '@/types';
 
 export const fetchProducts = createAsyncThunk<IProductsOfPage, string>(
   'products/fetchAll',
@@ -13,10 +13,21 @@ export const fetchProducts = createAsyncThunk<IProductsOfPage, string>(
 export const fetchProductsByCategory = createAsyncThunk<IProductsOfPage, IQueryObjectCategory>(
   'products/fetchByCategory',
   async (query) => {
-    const productsResponse = await axiosApi.get<IProductsOfPage>(`/products?categoryId=${query.categoryId}&categoryPage=${query.categoryPage}`);
+    const productsResponse = await axiosApi.get<IProductsOfPage>(
+      `/products?categoryId=${query.categoryId}&categoryPage=${query.categoryPage}`,
+    );
 
     return productsResponse.data;
-  });
+  },
+);
+
+export const fetchBestsellers = createAsyncThunk<IProduct[], string>(
+  'products/fetchByFilter',
+  async (query) => {
+    const productsResponse = await axiosApi.get<IProduct[]>(`/products?filterBy=${query}`);
+    return productsResponse.data;
+  },
+);
 
 // export const fetchProducts = createAsyncThunk<IProduct[]>('products/fetchAll', async () => {
 //   const productsResponse = await axiosApi.get<IProduct[]>('/products');
