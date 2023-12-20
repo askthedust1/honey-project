eb2ca306d13b57c5147ec57b8f728e6bbf676445
Victoria <124435946+askthedust1@users.noreply.github.com>
2023-12-20 15:46:52 +0600

    HONEY-195 add styles

--- front/src/components/RelatedProducts/RelatedProducts.tsx
+++ front/src/components/RelatedProducts/RelatedProducts.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IProduct } from '@/types';
 import ProductItem from '@/features/products/components/ProductItem';
 import cls from '../../styles/_product.module.scss';
+import otherCls from '@/styles/_products.module.scss';
 
 interface Props {
   products: IProduct[];
@@ -15,7 +16,7 @@ const RelatedProducts: React.FC<Props> = ({ products }) => {
           <h4 className={cls.relatedText}>Похожие товары:</h4>
           <div className={cls.relatedWrap}>
             {products.map((el: IProduct) => (
-              <ProductItem key={el._id} product={el} />
+              <ProductItem key={el._id} product={el} customClass={otherCls.card_block_white} />
             ))}
           </div>
         </div>
