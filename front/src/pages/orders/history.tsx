4c1716738c1bc54f87e850b4b86ae8268073a672
karinami <mkf1230@gmail.com>
2023-12-27 00:57:41 +0600

    HONEY-200 Added responsive for Orders History page

--- front/src/pages/orders/history.tsx
+++ front/src/pages/orders/history.tsx
@@ -10,7 +10,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import axiosApi from '@/axiosApi';
-import { fetchCategories } from '@/features/categories/categoriesThunk';
 
 const History: MyPage = () => {
   const { t, i18n } = useTranslation('ordersHistory');
@@ -25,66 +24,115 @@ const History: MyPage = () => {
     <div className={cls.history}>
       <Head>
         <title>{t('title')}</title>
-        <meta name="description" content="История покупок" />
+        <meta name="description" content="История заказов" />
       </Head>
       <h1 className={cls.history_mainTitle}>{t('title')}</h1>
       {!orders.length ? (
-        <div>
+        <div className={cls.history_noOrders}>
           <h2 className={cls.history_title}>{t('noOrders')}</h2>
           <div className={cls.history_return}>
             <Link href={'/products/page/1'}>{t('orderBtn')}</Link>
           </div>
         </div>
       ) : (
-        <div className={cls.adminBestsellersTable}>
-          <table>
-            <thead>
-              <tr>
-                <th>{t('numberOrder')}</th>
-                <th>{t('address')}</th>
-                <th>{t('date')}</th>
-                <th>{t('orderList')}</th>
-                <th>{t('total')}</th>
-                <th>{t('status')}</th>
-              </tr>
-            </thead>
-            <tbody className={cls.adminBestsellersTable_body}>
-              {orders.map((order) => (
-                <tr key={order.indexNumber}>
-                  <td>{order.indexNumber}</td>
-                  <td>{order.address}</td>
-                  <td>
-                    {new Date(order.dateTime).toLocaleString('en-US', {
-                      day: 'numeric',
-                      month: 'numeric',
-                      year: 'numeric',
-                      hour: 'numeric',
-                      minute: 'numeric',
-                      hour12: false,
-                      timeZone: 'UTC',
-                    })}
-                  </td>
-                  <td className={cls.prod}>
-                    {order.kits.map((i) => (
-                      <ul key={i.product._id} className={cls.list}>
-                        <li>
-                          {i.product.title} -{' '}
-                          <span>
-                            {i.amount} {t('piece')}
-                          </span>
-                        </li>
-                      </ul>
-                    ))}
-                  </td>
-                  <td>
-                    {order.totalPrice} {t('som')}
-                  </td>
-                  <td>{order.status ? t('statusYes') : t('statusNo')}</td>
+        <>
+          <div className={cls.historyTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>{t('numberOrder')}</th>
+                  <th>{t('address')}</th>
+                  <th>{t('date')}</th>
+                  <th>{t('orderList')}</th>
+                  <th>{t('total')}</th>
+                  <th>{t('status')}</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody className={cls.historyTable_body}>
+                {orders.map((order) => (
+                  <tr key={order.indexNumber}>
+                    <td>{order.indexNumber}</td>
+                    <td>{order.address}</td>
+                    <td>
+                      {new Date(order.dateTime).toLocaleString('en-US', {
+                        day: 'numeric',
+                        month: 'numeric',
+                        year: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        hour12: false,
+                        timeZone: 'UTC',
+                      })}
+                    </td>
+                    <td className={cls.prod}>
+                      {order.kits.map((i) => (
+                        <ul key={i.product._id} className={cls.list}>
+                          <li>
+                            {i.product.title} -{' '}
+                            <span>
+                              {i.amount} {t('piece')}
+                            </span>
+                          </li>
+                        </ul>
+                      ))}
+                    </td>
+                    <td>
+                      {order.totalPrice} {t('som')}
+                    </td>
+                    <td>{order.status ? t('statusYes') : t('statusNo')}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className={cls.ordersCards}>
+            {orders.map((order) => (
+              <div className={cls.ordersCards_card} key={order.indexNumber}>
+                <p>
+                  <span className={cls.ordersCards_card_marker}>{t('numberOrder')}:</span>{' '}
+                  {order.indexNumber}
+                </p>
+                <p>
+                  <span className={cls.ordersCards_card_marker}>{t('address')}:</span>{' '}
+                  {order.address}
+                </p>
+                <p>
+                  <span className={cls.ordersCards_card_marker}>{t('date')}:</span>{' '}
+                  {new Date(order.dateTime).toLocaleString('en-US', {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: false,
+                    timeZone: 'UTC',
+                  })}
+                </p>
+                <p>
+                  <span className={cls.ordersCards_card_marker}>{t('total')}:</span>{' '}
+                  {order.totalPrice} {t('som')}
+                </p>
+                <p>
+                  <span className={cls.ordersCards_card_marker}>{t('status')}:</span>{' '}
+                  {order.status ? t('statusYes') : t('statusNo')}
+                </p>
+                <p className={cls.ordersCards_card_order}>
+                  <span className={cls.ordersCards_card_marker}>{t('orderList')}:</span>{' '}
+                  {order.kits.map((i) => (
+                    <ul key={i.product._id} className={cls.list}>
+                      <li>
+                        {i.product.title} -{' '}
+                        <span>
+                          {i.amount} {t('piece')}
+                        </span>
+                      </li>
+                    </ul>
+                  ))}
+                </p>
+              </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
@@ -93,10 +141,8 @@ const History: MyPage = () => {
 History.Layout = 'Main';
 
 export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ locale }) => {
-  const lang = locale ?? 'ru';
-  axiosApi.defaults.headers.common['Accept-Language'] = lang;
+  axiosApi.defaults.headers.common['Accept-Language'] = locale ?? 'ru';
 
-  await store.dispatch(fetchCategories(lang));
   return {
     props: {
       ...(await serverSideTranslations(locale ?? 'ru', ['header', 'ordersHistory', 'footer'])),
