7160b515bb2a5eeff2252ad3b3f3efce712043ab
karinami <mkf1230@gmail.com>
2023-12-29 00:51:36 +0600

    HONEY-225 Added multilang logic in promotion.ts

--- api/routers/promotion.ts
+++ api/routers/promotion.ts
@@ -1,36 +1,47 @@
-import Product from "../models/Product";
-import express from "express";
+import Product from '../models/Product';
+import express from 'express';
+import { IProductPost } from '../types';
 
 const promotionRouter = express.Router();
 
 promotionRouter.get('/', async (req, res) => {
-    try {
-        let page1 = 1;
-        const perPage = 9;
-        const totalProducts = await Product.find({
-            $and: [{ $expr: { $ne: ['$oldPrice', '$actualPrice'] } }, { isActive: true }]
-        }).countDocuments();
-        const totalPages = Math.ceil(totalProducts / perPage);
+  const lang = req.headers['accept-language'] || 'ru';
 
-        page1 = parseInt(req.query.page as string);
+  try {
+    let page1 = 1;
+    const perPage = 9;
+    const totalProducts = await Product.find({
+      $and: [{ $expr: { $ne: ['$oldPrice', '$actualPrice'] } }, { isActive: true }],
+    }).countDocuments();
+    const totalPages = Math.ceil(totalProducts / perPage);
 
-        const result = await Product.find({
-            $and: [{ $expr: { $ne: ['$oldPrice', '$actualPrice'] } }, { isActive: true }]
-        })        .populate('category', 'title description')
-            .skip((page1 - 1) * perPage)
-            .limit(perPage);
+    page1 = parseInt(req.query.page as string);
 
-        const productsWithPages = {
-            productsOfPage: result,
-            currentPage: page1,
-            totalPages,
-        };
-        return res.send(productsWithPages);
+    const result = await Product.find({
+      $and: [{ $expr: { $ne: ['$oldPrice', '$actualPrice'] } }, { isActive: true }],
+    })
+      .populate('category', 'title description')
+      .skip((page1 - 1) * perPage)
+      .limit(perPage);
 
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        return res.status(500).send('Internal Server Error');
-    }
+    const fit = result.map((i) => {
+      const product = i.toObject() as IProductPost;
+      return {
+        ...product,
+        title: product.translations[lang].title,
+      };
+    });
+
+    const productsWithPages = {
+      productsOfPage: fit,
+      currentPage: page1,
+      totalPages,
+    };
+    return res.send(productsWithPages);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return res.status(500).send('Internal Server Error');
+  }
 });
 
 export default promotionRouter;
