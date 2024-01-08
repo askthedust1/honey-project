d8b3c12e8831ecfa8a77ac196b8fa00339fd18b4
Zobnin Boris <singulthor@gmail.com>
2024-01-08 15:59:10 +0600

    HONEY-222-Admin full functional with pagination (change after rollback)

---
+++ front/src/components/FilterBar/FilterBar.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import FilterBarItems from '@/components/FilterBar/FilterBarItems';
+import cls from '@/styles/_products.module.scss';
+import { useTranslation } from 'next-i18next';
+
+interface Props {
+  categoryId?: string;
+}
+
+const FilterBar: React.FC<Props> = ({ categoryId }) => {
+  const { t } = useTranslation('common');
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  return (
+    <div onClick={toggleDropdown} className={cls.dropdownContainer}>
+      <button className={cls.dropdownDangerToggle}>{t('filter4')} &#9662;</button>
+      {showDropdown && <FilterBarItems categoryId={categoryId} />}
+    </div>
+  );
+};
+
+export default FilterBar;
---
+++ front/src/pages/404.tsx
@@ -0,0 +1,11 @@
+import NotFound404 from '@/components/UI/notFound404/NotFound404';
+
+export default function Custom404(): JSX.Element {
+  return (
+    <div>
+      <NotFound404 />
+    </div>
+  );
+}
+
+Custom404.Layout = 'Main';
